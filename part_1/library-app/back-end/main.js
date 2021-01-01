bb884bb63db377fb0547ef465057d328b6808f24
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-01 22:11:19 +0200

    network fail

--- part_1/library-app/back-end/main.js
+++ part_1/library-app/back-end/main.js
@@ -1,48 +1,46 @@
-const Connection = require("../back-end/db")
+const Connection = require("../back-end/db");
 
-const createBook=(req,res)=>{
-const {title , author, pages, publisher, published_at}=req.body
-const data=[title , author, pages, publisher, published_at]
-const query=`INSERT INTO library (title , author, pages, publisher, published_at)VALUSE(?,?,?,?,?)`
-Connection.query(query,data,(err,results)=>{
+const createBook = (req, res) => {
+  const { title, author, pages, publisher, published_at } = req.body;
+  const data = [title, author, pages, publisher, published_at];
+  const query = `INSERT INTO library (title , author, pages, publisher, published_at)VALUSE(?,?,?,?,?)`;
+  Connection.query(query, data, (err, results) => {
     if (err) throw err;
-    res.json(results)
-})
-}
+    res.json(results);
+  });
+};
 
-const getBooks=(req,res)=>{
-    const query=`SELECT * FROM library`
-    Connection.query(query,data,(err,results)=>{
-        if (err) throw err;
-        res.json(results)
-    })
-    }
+const getBooks = (req, res) => {
+  const query = `SELECT * FROM library`;
+  Connection.query(query, data, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+};
 
-    const getSpecificBook=(req,res)=>{
-        const {book_id}=req.params.book_id
-        const query=`SELECT * FROM library WHERE book_id=?`
-        const data=[book_id]
-        Connection.query(query,data,(err,results)=>{
-            if (err) throw err;
-            res.json(results)
-        })
-        }
-    
-        
-        const deleteSpecificBook=(req,res)=>{
-            const {book_id}=req.params.book_id
-            const query=`DELETE * FROM library WHERE book_id=?`
-            const data=[book_id]
-            Connection.query(query,data,(err,results)=>{
-                if (err) throw err;
-                res.json(results)
-            })
-            }
-        
+const getSpecificBook = (req, res) => {
+  const { book_id } = req.params.book_id;
+  const query = `SELECT * FROM library WHERE book_id=?`;
+  const data = [book_id];
+  Connection.query(query, data, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+};
 
-module.exports={
-createBook,
-getBooks,
-getSpecificBook,
-deleteSpecificBook
-}
+const deleteSpecificBook = (req, res) => {
+  const { book_id } = req.params.book_id;
+  const query = `DELETE * FROM library WHERE book_id=?`;
+  const data = [book_id];
+  Connection.query(query, data, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+};
+
+module.exports = {
+  createBook,
+  getBooks,
+  getSpecificBook,
+  deleteSpecificBook,
+};
--- part_1/library-app/back-end/server.js
+++ part_1/library-app/back-end/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const {createBook,getBooks,getSpecificBook,deleteSpecificBook}=require("../back-end/main")
+const {
+  createBook,
+  getBooks,
+  getSpecificBook,
+  deleteSpecificBook,
+} = require("../back-end/main");
 const app = express();
 express.json();
 
-
-app.post("/books",createBook)
-app.get("/books",getBooks)
-app.get("/books/:book_id",getSpecificBook)
-app.delete("/books/:book_id",deleteSpecificBook)
-
-
+app.post("/books", createBook);
+app.get("/books", getBooks);
+app.get("/books/:book_id", getSpecificBook);
+app.delete("/books/:book_id", deleteSpecificBook);
 
 const port = 3000;
 app.listen(port, () => {
